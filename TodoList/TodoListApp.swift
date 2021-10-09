9066c418df46ca374ea6042fcb2bfdd082f2f584
victorlss <contact@victorleonardo.com>
2021-10-09 17:30:10 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Victor Souza da Silva on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
