4d7da8c5d73a2ecb9d4dea476e71e071399c0467
victorlss <contact@victorleonardo.com>
2021-10-10 12:35:31 -0300

    Save items in UserDefaults

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -7,37 +7,51 @@
 
 import Foundation
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    let itemsKey = "items_list"
+    
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
     
     init() {
-        loadSampleData()
+        loadData()
+    }
+    
+    func addItem(title: String) {
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
     }
     
     func deleteItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
     }
     
+    func loadData() {
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {
+           return
+        }
+        
+        items.append(contentsOf: savedItems)
+    }
+    
     func moveItem(from: IndexSet, to: Int) {
         items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
     }
     
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = ItemModel(title: item.title, isCompleted: !item.isCompleted)
         }
-    }
-    
-    func addItem(title: String) {
-        let newItem: ItemModel = ItemModel(title: title, isCompleted: false)
-        items.append(newItem)
-    }
-    
-    func loadSampleData() {
-        let sampleData = [
-            ItemModel(title: "My first item", isCompleted: false),
-            ItemModel(title: "My second item", isCompleted: true)
-        ]
-        
-        items.append(contentsOf: sampleData)
     }
 }
